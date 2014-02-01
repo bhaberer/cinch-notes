b1c58329052d9ee79dcc998a7bc4ecdb7d70cf40
Brian Haberer <bhaberer@gmail.com>
2014-01-31 16:43:57 -0800

    Fixin boogS

--- lib/cinch/plugins/notes.rb
+++ lib/cinch/plugins/notes.rb
@@ -1,8 +1,44 @@
 # -*- coding: utf-8 -*-
 require 'cinch'
+require 'cinch-storage'
 
 module Cinch::Plugins
   class Notes
+    include Cinch::Plugin
+
+    listen_to :channel,         method: :send_notes
+
+    set prefix: ''
+
+    match /\Atell (\w+) (.+)/,  react_on: :private,
+                                method: :make_note
+
+    def initialize(*args)
+      super
+      @storage = CinchStorage.new('yaml/notes.yml')
+      @storage.data ||= {}
+    end
+
+    def make_note(m, user, message)
+      note = Note.new(m.user.nick, user, message)
+      @storage.data[user.downcase] ||= []
+      @storage.data[user.downcase] << note
+      @storage.synced_save(@bot)
+      m.reply 'ok, I will let them know!'
+    end
+
+    def send_notes(m)
+      nick = m.user.nick.downcase
+      if @storage.data.key?(nick)
+        @storage.data[nick].each do |note|
+          unless note.sent
+            m.user.send("#{note.from} asked me to tell you '#{note.message}'")
+            note.mark_sent
+          end
+        end
+      end
+    end
+
     class Note
       attr_accessor :from, :to, :message, :time, :sent
 
@@ -18,28 +54,9 @@ module Cinch::Plugins
         { from: from, to: to, message: message, time: time, sent: sent }
       end
 
-      def send
-        User(@to).send("#{@from} asked me to tell you '#{@message}'")
+      def mark_sent
         @sent = true
       end
-    end
-
-    include Cinch::Plugin
-
-    set prefix: /\Atell/, react_on: :private
-
-    match /(\w+) (.+)/
-
-    def initialize(*args)
-      @storage = CinchStorage.new(config[:filename] || 'yaml/notes.yml')
-      @storage.data ||= {}
-    end
-
-    def execute(m, user, message)
-      note = Note.new(m.user.nick, user, message)
-      @storage.data << note
-      @storage.synced_save(@bot)
-      m.reply 'ok, I will let them know!'
     end
   end
 end
